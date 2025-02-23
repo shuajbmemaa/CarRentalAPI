5b70319af214ec7c0f7fbd705b95688fc60e6b69
shuajbkingu <shuajbmemaa@hotmail.com>
2025-02-23 15:02:40 +0100

    add models

---
+++ models/Car.js
@@ -0,0 +1,8 @@
+export const Car = {
+    name: String,
+    price_per_day: Number,
+    year: Number,
+    color: String,
+    steering_type: String,
+    number_of_seats: Number
+};
---
+++ models/User.js
@@ -0,0 +1,7 @@
+export const User = {
+    fullName: String,
+    email: String,
+    username: String,
+    password: String,
+    createdAt: Date
+};
