ccfbb68789ba59f052ded329890b3b32d40390eb
shuajbkingu <shuajbmemaa@hotmail.com>
2025-02-24 12:06:26 +0100

    add endpoint for deleting a car by ID

--- routes/cars.js
+++ routes/cars.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { getRentalCars } from '../controllers/cars.js';
-import { createCar } from '../controllers/cars.js'
+import { getRentalCars, createCar, deleteCarById } from '../controllers/cars.js'
 
 const router = express.Router();
 
 router.get('/rental-cars', getRentalCars);
 router.post('/insertCar', createCar);
+router.delete('/deleteCar/:id', deleteCarById)
 
 export default router;
